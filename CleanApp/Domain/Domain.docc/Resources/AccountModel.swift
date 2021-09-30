0f4176f866494913ab7a64449388e1ba2707cf87
Marcelo Garcia <ti@mobo.com.br>
2021-09-30 19:56:28 -0300

    first commit

---
+++ CleanApp/Domain/Domain.docc/Resources/AccountModel.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by Marcelo Garcia on 30/09/21.
+//
+
+import Foundation
+
+struct AccountModel {
+    var id: String
+    var name: String
+    var email: String
+    var password: String
+}
