ab45614be4179e800410b9c7ba35241eb1a2d42d
sagarcia31 <marcelo.sagarcia2@gmail.com>
2021-10-05 01:07:07 -0300

    rename test name

--- CleanApp/DataTests/RemoteAddAccountTests.swift
+++ CleanApp/DataTests/RemoteAddAccountTests.swift
@@ -27,7 +27,7 @@ protocol HttpPostClient {
 }
 
 class RemoteAddAccountTests: XCTestCase {
-    func test_(){
+    func test_add_should_call_httpClient_with_correct_url(){
         let url = URL(string:"http://any-url.com")!
         let httpClientSpy = HttpClientSpy()
         // serve para identificar a classe que estou testando
