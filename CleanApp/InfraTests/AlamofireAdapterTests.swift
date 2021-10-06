8c923cde83d3512f408f49d9bd2221e522660b7d
sagarcia31 <marcelo.sagarcia2@gmail.com>
2021-10-06 11:31:52 -0300

    add infra layer

---
+++ CleanApp/InfraTests/AlamofireAdapterTests.swift
@@ -0,0 +1,18 @@
+//
+//  InfraTests.swift
+//  InfraTests
+//
+//  Created by Marcelo Garcia on 06/10/21.
+//
+
+import XCTest
+@testable import Infra
+
+class InfraTests: XCTestCase {
+    
+    func test_()  {
+        
+    }
+    
+    
+}
