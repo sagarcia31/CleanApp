46cb0540b96942aee7b85fe8abe8bbc713f0be16
sagarcia31 <marcelo.sagarcia2@gmail.com>
2021-10-06 01:36:10 -0300

    Testing Remote account to return with data.

---
+++ CleanApp/Data/Helpers/ExtensionHelpers.swift
@@ -0,0 +1,13 @@
+//
+//  ExtensionHelpers.swift
+//  Data
+//
+//  Created by Marcelo Garcia on 06/10/21.
+//
+
+import Foundation
+public extension Data {
+    func toModel<T: Decodable>() -> T? {
+        return try? JSONDecoder().decode(T.self, from: self)
+    }
+}
--- CleanApp/DataTests/UseCases/DataTests.swift
+++ CleanApp/DataTests/UseCases/DataTests.swift
@@ -14,13 +14,13 @@ class RemoteAddAccountTests: XCTestCase {
     func test_add_should_call_httpClient_with_correct_url(){
         let url = URL(string:"http://any-url.com")!
         let (sut, httpClientSpy) = makeSut()
-        sut.add(addAccountModel: makeAccountModel()) {_ in}
+        sut.add(addAccountModel: makeAddAccountModel()) {_ in}
         XCTAssertEqual(httpClientSpy.urls, [url])
     }
     
     func test_add_should_call_httpClient_with_correct_data(){
         let (sut, httpClientSpy) = makeSut()
-        let addAccountModel = makeAccountModel()
+        let addAccountModel = makeAddAccountModel()
         sut.add(addAccountModel: addAccountModel) {_ in}
         XCTAssertEqual(httpClientSpy.data, addAccountModel.toData())
     }
@@ -28,7 +28,7 @@ class RemoteAddAccountTests: XCTestCase {
     func test_add_should_complete_with_error_if_client_completes_with_error(){
         let (sut, httpClientSpy) = makeSut()
         let exp = expectation(description: "waiting")
-        sut.add(addAccountModel: makeAccountModel()) { result in
+        sut.add(addAccountModel: makeAddAccountModel()) { result in
             switch result {
             case .failure(let error):XCTAssertEqual(error, .unexpected)
             case .success: XCTFail("Expected error receive \(result) instead")
@@ -36,6 +36,22 @@ class RemoteAddAccountTests: XCTestCase {
             exp.fulfill()
         }
         httpClientSpy.completeWithError(.noConnectivity)
+        wait(for: [exp], timeout: 1)
+    }
+    
+    func test_add_should_complete_with_error_if_client_completes_with_data(){
+        let (sut, httpClientSpy) = makeSut()
+        let exp = expectation(description: "waiting")
+        let expextedAccount = makeAccountModel()
+        
+        sut.add(addAccountModel: makeAddAccountModel()) { result in
+            switch result {
+            case .failure: XCTFail("Expected success receive \(result) instead")
+            case .success(let receivedAccount): XCTAssertEqual(receivedAccount, expextedAccount)
+            }
+            exp.fulfill()
+        }
+        httpClientSpy.completeWithData(expextedAccount.toData()!)
         wait(for: [exp], timeout: 1)
     }
 }
@@ -49,8 +65,12 @@ extension RemoteAddAccountTests {
     }
     
     //Factory para criaçao de conta
-    func makeAccountModel() -> AddAccountModel {
+    func makeAddAccountModel() -> AddAccountModel {
         return AddAccountModel(name: "any_name", email: "any_email@email.com", password: "any_password", passwordConfirmation: "any_password")
+    }
+    
+    func makeAccountModel() -> AccountModel {
+        return AccountModel(id:"1", name: "any_name", email: "any_email@email.com", password: "any_password")
     }
     
     // Faz uma implementaçao fake do protocolo httpclient
@@ -67,6 +87,10 @@ extension RemoteAddAccountTests {
         
         func completeWithError(_ error: HttpError) {
             completion?(.failure(error))
+        }
+        
+        func completeWithData(_ data: Data) {
+            completion?(.success(data))
         }
     }
 }
