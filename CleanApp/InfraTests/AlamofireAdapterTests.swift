bd56e938ca3039fcd1f1fb2f63a43c56a92ea2a4
sagarcia31 <marcelo.sagarcia2@gmail.com>
2021-10-19 14:25:32 -0300

    passing parameters on json request

--- CleanApp/InfraTests/AlamofireAdapterTests.swift
+++ CleanApp/InfraTests/AlamofireAdapterTests.swift
@@ -16,8 +16,9 @@ class AlamofireAdapter {
         self.session = session
     }
     
-    func post(to url:URL) {
-        session.request(url, method: .post).resume()
+    func post(to url:URL, with data: Data?) {
+        let json = try? JSONSerialization.jsonObject(with: data!, options: .fragmentsAllowed) as? [String: Any]
+        session.request(url, method: .post, parameters: json, encoding: JSONEncoding.default).resume()
     }
 }
 
@@ -29,13 +30,14 @@ class InfraTests: XCTestCase {
         configuration.protocolClasses = [URLProtocolStub.self]
         let session = Session(configuration: configuration)
         let sut = AlamofireAdapter(session: session)
-        sut.post(to: url)
+        sut.post(to: url, with: makeValidData())
         let exp = expectation(description: "waiting")
         
         //Observable para verificar quando a request é feita
         URLProtocolStub.observeRequest{request in
             XCTAssertEqual(url, request.url)
             XCTAssertEqual("POST", request.httpMethod)
+            XCTAssertNotNil(request.httpBodyStream)
             exp.fulfill()
         }
         wait(for: [exp], timeout: 1)
