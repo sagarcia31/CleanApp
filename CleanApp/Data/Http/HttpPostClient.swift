24aad206f04577a22e73eb750ca5fd548a767719
sagarcia31 <marcelo.sagarcia2@gmail.com>
2021-11-19 20:00:56 -0300

    all scenarios from response api tested

--- CleanApp/Data/Http/HttpPostClient.swift
+++ CleanApp/Data/Http/HttpPostClient.swift
@@ -9,5 +9,5 @@ import Foundation
 
 // interface segregation principle para ter pequenauus interfaces por protocolo
 public protocol HttpPostClient {
-    func post(to url: URL, with data: Data?, completion: @escaping (Result<Data,HttpError>) -> Void)
+    func post(to url: URL, with data: Data?, completion: @escaping (Result<Data?,HttpError>) -> Void)
 }
